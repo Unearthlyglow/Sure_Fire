c14798960df18f861cf7ed4f6111fa5cd1ea52e9
Award <awardmalisi@gmail.com>
2023-05-16 00:18:08 -0500

    vercel adapter added

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import adapter from '@sveltejs/adapter-vercel';
 
 export default defineConfig({
+	kit: {
+		adapter: adapter({
+				// see the 'Deployment configuration' section below
+		})
+},
 	plugins: [sveltekit()]
+	
 });
